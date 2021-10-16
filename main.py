eccee975055619f83e42a0162f4d17985f1b6772
userbot8808 <92530494+userbot8808@users.noreply.github.com>
2021-10-16 14:22:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types.message import ParseMode
 
-bot = Bot(token="2063496925:AAHXTCV2ofew-xRdKf_z8vufFTBW4cd75pY", parse_mode=ParseMode.MARKDOWN_V2)
+bot = Bot(token="BOT_TOKEN", parse_mode=ParseMode.MARKDOWN_V2)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
